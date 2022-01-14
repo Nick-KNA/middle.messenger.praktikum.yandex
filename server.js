bf0cb75b69e260fc074e45ed1f1ffe41a34467fa
Короид Николай Андреевич <koroid.n.an@sberbank.ru>
2022-01-14 14:55:11 +0300

    Build added to npm run start

--- server.js
+++ server.js
@@ -5,10 +5,6 @@ const PORT = 3000;
 
 app.use(express.static(path.join(__dirname + '/dist')));
 
-app.use('*', function (req, res) {
-	res.sendFile(path.join(__dirname, './dist/index.html'));
-});
-
 app.listen(PORT, function () {
 	console.log(`Chat App server started on port ${PORT}!`);
 });
