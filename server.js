efe1148b9de127803cbd6abb4536d8ce24b990ee
Короид Николай Андреевич <koroid.n.an@sberbank.ru>
2022-01-14 14:45:41 +0300

    Trying to fix server.js

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(path.join(__dirname + '/dist')));
+
+app.use('*', function (req, res) {
+	res.sendFile(path.join(__dirname, './dist/index.html'));
+});
 
 app.listen(PORT, function () {
 	console.log(`Chat App server started on port ${PORT}!`);
